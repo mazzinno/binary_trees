0270e75e5b3dcec8644c98a0ab61a863672152c3
mazzinno <abdennasr3@gmail.com>
2024-01-06 02:07:12 +0100

    test

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_preorder - pre-order traversing
  *
- * @tree: ptr to the root node of the tree to traverse. 
+ * @tree: ptr to the root node of the tree to traverse.
  * @func: ptr to a function to call for each node...
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
